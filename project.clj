5cfb57624af6c30a62caa5da82021aef01165423
Joona Haavisto <joona.haavisto@gofore.com>
2018-07-23 14:57:59 +0300

    Lisätty cloverage

--- project.clj
+++ project.clj
@@ -33,10 +33,12 @@
   :plugins [[lein-ring "0.12.4"]
             [lein-environ "1.1.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]
-                   :plugins [[lein-midje "3.2"]]}
+                   :plugins [[lein-midje "3.2"]
+                             [lein-cloverage "1.0.11" :exclusions [org.clojure/clojure]]]}
              :test {:dependencies [[ring/ring-mock "0.3.2"]
                                    [pl.allegro.tech/embedded-elasticsearch "2.4.2"]]}
              :uberjar {:ring {:port 8080}}}
   :aliases {"run" ["ring" "server" "3006"]
             "uberjar" ["do" "clean" ["ring" "uberjar"]]
-            "test" ["with-profile" "+test" "midje"]})
+            "test" ["with-profile" "+test" "midje"]
+            "cloverage" ["with-profile" "+test" "cloverage" "--runner" ":midje"]})
