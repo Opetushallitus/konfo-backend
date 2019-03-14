69a2e89ed00eefbbcb5b7df7416f8738a61e8383
Jessika Penttinen <jessika.penttinen@siili.com>
2019-03-14 15:41:50 +0200

    Nostettu riippuvuuksien ja leinin versioita

--- project.clj
+++ project.clj
@@ -3,27 +3,27 @@
   :repositories [["oph-releases" "https://artifactory.opintopolku.fi/artifactory/oph-sade-release-local"]
                  ["oph-snapshots" "https://artifactory.opintopolku.fi/artifactory/oph-sade-snapshot-local"]
                  ["ext-snapshots" "https://artifactory.opintopolku.fi/artifactory/ext-snapshot-local"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  ; Rest + server
                  [metosin/compojure-api "1.1.11"]
-                 [compojure "1.6.0"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]
-                 [ring-cors "0.1.11"]
+                 [compojure "1.6.1"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring-cors "0.1.13"]
                  ; Logging
-                 [oph/clj-log "0.1.0-SNAPSHOT"]
-                 [oph/clj-test-utils "0.1.0-SNAPSHOT"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [org.apache.logging.log4j/log4j-api "2.9.0"]
-                 [org.apache.logging.log4j/log4j-core "2.9.0"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.9.0"]
+                 [oph/clj-log "0.2.0-SNAPSHOT"]
+                 [oph/clj-test-utils "0.2.0-SNAPSHOT"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [org.apache.logging.log4j/log4j-api "2.11.1"]
+                 [org.apache.logging.log4j/log4j-core "2.11.1"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.11.1"]
                  [clj-log4j2 "0.2.0"]
                  ; Configuration
                  [fi.vm.sade.java-utils/java-properties "0.1.0-SNAPSHOT"]
                  [environ "1.1.0"]
-                 [cprop "0.1.10"]
+                 [cprop "0.1.13"]
                  ; Elasticsearch
-                 [oph/clj-elasticsearch "0.1.0-SNAPSHOT"]]
+                 [oph/clj-elasticsearch "0.2.0-SNAPSHOT"]]
   :ring {:handler konfo-backend.core/app
          :init konfo-backend.core/init
          ;:destroy konfo-backend.core/destroy
@@ -33,7 +33,7 @@
   :target-path "target/%s"
   :plugins [[lein-ring "0.12.4"]
             [lein-environ "1.1.0"]]
-  :profiles {:dev {:plugins [[lein-cloverage "1.0.11" :exclusions [org.clojure/clojure]]]}
+  :profiles {:dev {:plugins [[lein-cloverage "1.0.13" :exclusions [org.clojure/clojure]]]}
              :test {:dependencies [[ring/ring-mock "0.3.2"]]}
              :ci-test {:dependencies [[ring/ring-mock "0.3.2"]] :jvm-opts ["-Dlog4j.configurationFile=test/resources/log4j2.properties" "-Dconf=ci-configuration/konfo-backend.edn"]}
              :uberjar {:ring {:port 8080}}}
