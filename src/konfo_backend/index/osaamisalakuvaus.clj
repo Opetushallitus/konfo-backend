491083f3a45c0b5d0ec1669a37d6edbf27255430
Jessika Penttinen <jessika.penttinen@siili.com>
2019-04-18 12:17:34 +0300

    Lisätty mahdollisuus hakea osaamisalojen kuvauksen epeurtekuvauksen kanssa

---
+++ src/konfo_backend/index/osaamisalakuvaus.clj
@@ -0,0 +1,31 @@
+(ns konfo-backend.index.osaamisalakuvaus
+  (:require
+    [konfo-backend.tools :refer [koodi-uri-no-version]]
+    [konfo-backend.elastic-tools :refer [get-source search]]))
+
+;TODO tilan pitäisi olla "julkaistu" eikä "valmis"
+
+(defonce index "osaamisalakuvaus")
+
+(def osaamisalakuvaus-search (partial search index))
+
+(defn get
+  [id]
+  (let [osaamisalakuvaus (get-source index id)]
+    (when (some-> osaamisalakuvaus :tila (= "valmis"))
+      osaamisalakuvaus)))
+
+
+(defn- kuvaus-result-mapper
+  [result]
+  (map (fn [source] {:nimi               (get-in source [:osaamisala :nimi])
+                     :osaamisalakoodiUri (get-in source [:osaamisala :uri])
+                     :id                 (:id source)
+                     :kuvaus             (:teksti source)}) (map :_source (some-> result :hits))))
+
+(defn get-kuvaukset-by-eperuste-id
+  [eperuste-id]
+  (osaamisalakuvaus-search kuvaus-result-mapper
+                           :_source [:id, :osaamisala.nimi, :osaamisala.uri, :teksti.fi, :teksti.sv, :teksti.en]
+                           :query {:bool {:must {:term {:eperuste-oid eperuste-id}},
+                                          :filter {:term {:tila "valmis"}}}}))
--- test/konfo_backend/index/eperuste_test.clj
+++ test/konfo_backend/index/eperuste_test.clj
@@ -24,16 +24,42 @@
                                            :id 3536456,
                                            :kuvaus {:fi "kuvaus fi"
                                                     :sv "kuvaus sv"}}}]}})
+
+(defonce mocked-osaamisala-response
+  {:hits {:total 2
+          :hits [{:_source {:id 123 :osaamisala {:nimi {:fi "Osaamisala 1 nimi fi" :sv "Osaamisala 1 nimi sv"} :uri "osaamisala_1"} :teksti {:fi "Osaamisala 1 teksti fi" :sv "Osaamisala 1 teksti sv"}}},
+                 {:_source {:id 124 :osaamisala {:nimi {:fi "Osaamisala 2 nimi fi" :sv "Osaamisala 2 nimi sv"} :uri "osaamisala_2"} :teksti {:fi "Osaamisala 2 teksti fi" :sv "Osaamisala 2 teksti sv"}}}]}})
+
 (deftest eperuste-test
-  (testing "Get eperuste kuvaus"
+  (testing "Get eperuste-kuvaus"
     (with-redefs [clj-elasticsearch.elastic-connect/search (fn [x y & z] mocked-search-response)]
-      (let [response (get-ok (kuvaus-url "koulutus_355201#1"))]
+      (let [response (get-ok (kuvaus-url "koulutus_355201%231"))]
         (is (= response {:nimi {:fi "Ammattisukeltajan ammattitutkinto", :sv "Yrkesexamen för yrkesdykare",
                                 :en "Further vocational qualification for Commercial Divers"},
                          :koulutuskoodiUri "koulutus_355201",
                          :id 3536456,
                          :kuvaus {:fi "kuvaus fi"
                                   :sv "kuvaus sv"}})))))
+
+  (testing "Get eperuste-kuvaus with osaamisalakuvaukset"
+    (with-redefs [clj-elasticsearch.elastic-connect/search (fn [i y & z] (if (= "eperuste" i)
+                                                                           mocked-search-response
+                                                                           mocked-osaamisala-response))]
+      (let [response (get-ok (str (kuvaus-url "koulutus_355201%231") "?osaamisalakuvaukset=true"))]
+        (is (= response {:nimi {:fi "Ammattisukeltajan ammattitutkinto", :sv "Yrkesexamen för yrkesdykare",
+                                :en "Further vocational qualification for Commercial Divers"},
+                         :koulutuskoodiUri "koulutus_355201",
+                         :id 3536456,
+                         :kuvaus {:fi "kuvaus fi"
+                                  :sv "kuvaus sv"}
+                         :osaamisalat [{:nimi {:fi "Osaamisala 1 nimi fi" :sv "Osaamisala 1 nimi sv"}
+                                        :osaamisalakoodiUri "osaamisala_1"
+                                        :id 123
+                                        :kuvaus {:fi "Osaamisala 1 teksti fi" :sv "Osaamisala 1 teksti sv"}},
+                                       {:nimi {:fi "Osaamisala 2 nimi fi" :sv "Osaamisala 2 nimi sv"}
+                                        :osaamisalakoodiUri "osaamisala_2"
+                                        :id 124
+                                        :kuvaus {:fi "Osaamisala 2 teksti fi" :sv "Osaamisala 2 teksti sv"}}]})))))
 
   (testing "Get valmis eperuste"
     (with-redefs [clj-elasticsearch.elastic-connect/get-document (fn [x y z] {:found true :_source {:id 3536456 :tila "valmis"}})]
