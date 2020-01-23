a18feae2f61a5c79feebc1986bbc2ca87ae7307f
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-01-23 23:19:01 +0200

    updater smoke tests

---
+++ test/konfo_backend/contentful/updater_test.clj
@@ -0,0 +1,16 @@
+(ns konfo-backend.contentful.updater-test
+  (:require [clojure.test :refer :all]
+            [clj-elasticsearch.elastic-utils :refer [elastic-post]]
+            [konfo-backend.contentful.updater-api :refer :all]
+            [ring.mock.request :as mock]))
+
+(intern 'clj-log.access-log 'service "konfo-backend")
+
+(deftest updater-test
+  (testing "Healthcheck API test"
+    (let [response (updater-app (mock/request :get "/konfo-backend-updater/healthcheck"))]
+      (is (= (:status response) 200))))
+
+  (testing "Start update"
+    (let [response (updater-app (mock/request :post "/konfo-backend-updater/update"))]
+      (is (= (:status response) 200)))))
