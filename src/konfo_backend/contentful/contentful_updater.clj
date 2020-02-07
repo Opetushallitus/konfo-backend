5796146f25143243b1801961796ec4ef6fbb6f8f
Jussi Jartamo <jussi.jartamo@gofore.com>
2020-02-07 11:06:49 +0200

    always create asset

--- src/konfo_backend/contentful/contentful_updater.clj
+++ src/konfo_backend/contentful/contentful_updater.clj
@@ -191,11 +191,12 @@
             (log/info (str "MD5 " old-md5 (if (= old-md5 md5) " == " " != ") md5))
             (let [[image content-type] (fetch->image (or transformed original))]
               (store->s3 s3-client ttl-asset content-type s3-url image)))))
-      (if (= existing latest)
+      (comment (= existing latest)
         (if (nil? latest)
           (log/error (str "No asset found for locale " locale " in space " (:contentful-space-id config) " with token " (:contentful-access-token config)))
           (log/warn (str "Existing asset in bucket " (-> config :s3 :bucket-name) " in url " existing-url " is identical to new asset from space " (:contentful-space-id config))))
-        (store->s3 s3-client ttl-asset "application/json; charset=utf-8" key (.getBytes latest)))
+        )
+      (store->s3 s3-client ttl-asset "application/json; charset=utf-8" key (.getBytes latest))
       key)))
 
 (defn content-store-handler [client s3-client gson _ content-type locale base-url existing-url]
