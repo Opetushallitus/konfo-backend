c78aa9ba127125b039569e6c8cd5f8c9c8c52c1c
Jessika Penttinen <jessika.penttinen@siili.com>
2020-06-15 13:59:50 +0300

    Implementoitu rajapinta käännöksille

---
+++ test/konfo_backend/index/lokalisointi_test.clj
@@ -0,0 +1,15 @@
+(ns konfo-backend.index.lokalisointi-test
+  (:require [clojure.test :refer :all]
+            [konfo-backend.test-tools :refer :all]
+            [clj-log.access-log]))
+
+(intern 'clj-log.access-log 'service "konfo-backend")
+
+(deftest lokalisointi-test
+  (testing "Get lokalisointi"
+    (with-redefs [clj-elasticsearch.elastic-connect/get-document (fn [x y] {:found true,
+                                                                            :_source {:lng y
+                                                                                      :tyyppi "lokalisointi"
+                                                                                      :translation {:moi "moi"}}})]
+      (let [response (get-ok "/konfo-backend/translation/fi")]
+        (is (= response {:moi "moi"}))))))
