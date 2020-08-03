101912e74644ddd3593d665720e7c57fa25286d0
Jessika Penttinen <jessika.penttinen@siili.com>
2020-08-03 16:30:09 +0300

    Poistettu kuollutta koodia

--- src/konfo_backend/elastic_tools.clj
+++ src/konfo_backend/elastic_tools.clj
@@ -11,19 +11,6 @@
     (when (:found result)
       (:_source result))))
 
-(defn insert-query-perf [query duration started res-size]
-  (e/create
-    "query_perf"
-    {:created        (System/currentTimeMillis)
-     :started        started
-     :duration_mills duration
-     :query          query
-     :response_size  res-size}))
-
-(defn query-perf-string [type keyword constraints]
-  (println [type keyword (:koulutustyyppi constraints) (:paikkakunta constraints)])
-  (clojure.string/join "/" (remove clojure.string/blank? [type keyword (:koulutustyyppi constraints) (:paikkakunta constraints)])))
-
 (defn search
   [index mapper & query-parts]
   (->> (apply e/search
@@ -44,45 +31,6 @@
   (let [size (->size size)
         from (->from page size)]
     (apply search index mapper :from from :size size query-parts)))
-
-(defn old-search
-  [index perf-log-msg page size mapper & query-parts]
-  (log/debug query-parts)
-  (with-error-logging
-    (let [start (System/currentTimeMillis)
-          size  (->size size)
-          from  (->from page size)
-            res (->> (apply e/search
-                       index
-                       :from from
-                       :size size
-                       query-parts)
-                       :hits
-                       (mapper))]
-      (insert-query-perf perf-log-msg (- (System/currentTimeMillis) start) start (count res))
-      res)))
-
-(defn multi_match [keyword fields]
-  {:multi_match { :query keyword,
-                 :fields fields,
-                 :operator "and" }})
-
-(defn constant_score_query_multi_match [keyword fields boost]
-  { :constant_score {
-                     :filter { :multi_match { :query keyword
-                                             :fields fields
-                                             :operator "and" }},
-                     :boost boost }})
-
-(defn constant_score_query_match_tyypit [keyword boost]
-  { :constant_score {
-                     :filter { :match { :tyypit { :query keyword }}},
-                     :boost boost }})
-
-(defn constant_score_query_terms [key values boost]         ;:oid (vec oids)
-  { :constant_score {
-                     :filter { :terms { key values }},
-                     :boost boost }})
 
 (defn- virkailija-alias?
   [alias]
