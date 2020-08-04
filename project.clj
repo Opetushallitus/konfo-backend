42a9d716337e51b887f228e2fb340450219286cb
Topias Heinonen <topias.heinonen@gofore.com>
2020-08-04 14:44:25 +0300

    KTO-720 Päivitetty kouta-backend-riippuvuudet

--- project.clj
+++ project.clj
@@ -47,17 +47,19 @@
                    :jvm-opts ["-Dport=3006"]}
              :updater {:jvm-opts ["-Dmode=updater" "-Dport=3006"]}
              :test {:dependencies [[ring/ring-mock "0.3.2"]
-                                   [kouta-indeksoija-service "0.4.6-KTO-825-SNAPSHOT"]
-                                   [fi.oph.kouta/kouta-backend "0.27-SNAPSHOT"]
-                                   [fi.oph.kouta/kouta-backend "0.27-SNAPSHOT" :classifier "tests"]
+                                   [kouta-indeksoija-service "0.4.6-SNAPSHOT"]
+                                   [fi.oph.kouta/kouta-backend "1.1.0-SNAPSHOT"]
+                                   [fi.oph.kouta/kouta-backend "1.1.0-SNAPSHOT" :classifier "tests"]
+                                   [fi.oph.kouta/kouta-common "1.1.0-SNAPSHOT" :classifier "tests"]
                                    [org.mockito/mockito-core "2.28.2"]
                                    [oph/clj-test-utils "0.2.7-SNAPSHOT"]]
                     :injections [(require '[clj-test-utils.elasticsearch-mock-utils :as utils])
                                  (utils/global-elasticsearch-fixture)]}
              :ci-test {:dependencies [[ring/ring-mock "0.3.2"]
-                                      [kouta-indeksoija-service "0.4.6-KTO-825-SNAPSHOT"]
-                                      [fi.oph.kouta/kouta-backend "0.27-SNAPSHOT"]
-                                      [fi.oph.kouta/kouta-backend "0.27-SNAPSHOT" :classifier "tests"]
+                                      [kouta-indeksoija-service "0.4.6-SNAPSHOT"]
+                                      [fi.oph.kouta/kouta-backend "1.1.0-SNAPSHOT"]
+                                      [fi.oph.kouta/kouta-backend "1.1.0-SNAPSHOT" :classifier "tests"]
+                                      [fi.oph.kouta/kouta-common "1.1.0-SNAPSHOT" :classifier "tests"]
                                       [org.mockito/mockito-core "2.28.2"]
                                       [oph/clj-test-utils "0.2.7-SNAPSHOT"]]
                        :injections [(require '[clj-test-utils.elasticsearch-mock-utils :as utils])
