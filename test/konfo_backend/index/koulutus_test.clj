1c00460f88d8be2970deabf04e6c97d23fd2a32d
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-02-09 14:12:16 +0200

    KTO-443: Testi koulutuksen esikatselu true:lle

--- test/konfo_backend/index/koulutus_test.clj
+++ test/konfo_backend/index/koulutus_test.clj
@@ -21,21 +21,26 @@
 
   (let [koulutusOid1 "1.2.246.562.13.000001"
         koulutusOid2 "1.2.246.562.13.000002"
-        koulutusOid3 "1.2.246.562.13.000003"]
+        koulutusOid3 "1.2.246.562.13.000003"
+        koulutusOid4 "1.2.246.562.13.000004"]
 
-    (fixture/add-koulutus-mock koulutusOid1 :tila "julkaistu" :nimi "Hauska koulutus" :organisaatio mocks/Oppilaitos1)
-    (fixture/add-koulutus-mock koulutusOid2 :tila "tallennettu" :nimi "Hupaisa julkaisematon koulutus" :organisaatio mocks/Oppilaitos2)
+    (fixture/add-koulutus-mock koulutusOid1 :tila "julkaistu" :esikatselu "false" :nimi "Hauska koulutus" :organisaatio mocks/Oppilaitos1)
+    (fixture/add-koulutus-mock koulutusOid2 :tila "tallennettu" :esikatselu "false" :nimi "Hupaisa julkaisematon koulutus" :organisaatio mocks/Oppilaitos2)
+    (fixture/add-koulutus-mock koulutusOid4 :tila "tallennettu" :esikatselu "true" :nimi "Esikatselu sallittu" :organisaatio mocks/Oppilaitos2)
 
     (fixture/add-toteutus-mock "1.2.246.562.17.000001" koulutusOid1 :tila "julkaistu")
     (fixture/add-toteutus-mock "1.2.246.562.17.000002" koulutusOid1 :tila "tallennettu")
     (fixture/add-toteutus-mock "1.2.246.562.17.000003" koulutusOid1 :tila "julkaistu")
 
-    (fixture/index-oids-without-related-indices {:koulutukset [koulutusOid1 koulutusOid2]})
+    (fixture/index-oids-without-related-indices {:koulutukset [koulutusOid1 koulutusOid2 koulutusOid4]})
 
     (testing "Get koulutus"
       (testing "ok"
         (let [response (get-ok (koulutus-url koulutusOid1))]
           (is (= koulutusOid1 (:oid response)))))
+      (testing "allowed to get tallennettu when esikatselu true"
+        (let [response (get-ok (koulutus-draft-url koulutusOid4))]
+          (is (= koulutusOid4 (:oid response)))))
       (testing "filter julkaisemattomat toteutukset"
         (let [response (get-ok (koulutus-url koulutusOid1))]
           (is (= 2 (count (:toteutukset response))))))
