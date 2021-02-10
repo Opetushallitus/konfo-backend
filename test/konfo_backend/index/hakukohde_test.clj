77a29180c451c3955aacdc7861b8058b94877e06
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-02-10 13:22:32 +0200

    KTO-443: Testi hakukohteen draftille kun tallennettu ja esikatselu päällä

--- test/konfo_backend/index/hakukohde_test.clj
+++ test/konfo_backend/index/hakukohde_test.clj
@@ -13,12 +13,17 @@
   [oid]
   (apply url-with-query-params (str "/konfo-backend/hakukohde/" oid) [:draft false]))
 
+(defn hakukohde-draft-url
+  [oid]
+  (apply url-with-query-params (str "/konfo-backend/hakukohde/" oid) [:draft true]))
+
 (deftest hakukohde-test
 
   (let [hakukohdeOid1 "1.2.246.562.20.000001"
         hakukohdeOid2 "1.2.246.562.20.000002"
         hakukohdeOid3 "1.2.246.562.20.000003"
         hakukohdeOid4 "1.2.246.562.20.000004"
+        hakukohdeOid5 "1.2.246.562.20.000005"
         valintaperusteId1 "2d0651b7-cdd3-463b-80d9-303a60d9616c"
         valintaperusteId2 "45d2ae02-9a5f-42ef-8148-47d07737927b"]
 
@@ -29,6 +34,7 @@
     (fixture/add-hakukohde-mock hakukohdeOid1 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "julkaistu" :esikatselu "false" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId1)
     (fixture/add-hakukohde-mock hakukohdeOid2 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "julkaistu" :esikatselu "false" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId2)
     (fixture/add-hakukohde-mock hakukohdeOid3 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "tallennettu" :esikatselu "false" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId1)
+    (fixture/add-hakukohde-mock hakukohdeOid5 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "tallennettu" :esikatselu "true" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId2)
 
     (fixture/add-valintaperuste-mock valintaperusteId1 :tila "julkaistu" :esikatselu "false")
     (fixture/add-valintaperuste-mock valintaperusteId2 :tila "tallennettu" :esikatselu "false")
@@ -37,12 +43,15 @@
     (fixture/add-koulutus-mock "1.2.246.562.13.000001")
     (fixture/add-haku-mock "1.2.246.562.29.000001")
 
-    (fixture/index-oids-without-related-indices {:hakukohteet [hakukohdeOid1 hakukohdeOid2 hakukohdeOid3]})
+    (fixture/index-oids-without-related-indices {:hakukohteet [hakukohdeOid1 hakukohdeOid2 hakukohdeOid3 hakukohdeOid5]})
 
     (testing "Get haku"
       (testing "ok"
         (let [response (get-ok (hakukohde-url hakukohdeOid1))]
           (is (= hakukohdeOid1 (:oid response)))))
+      (testing "allowed to get draft hakukohde when tallennettu and esikatselu true"
+        (let [response (get-ok (hakukohde-draft-url hakukohdeOid5))]
+          (is (= hakukohdeOid5 (:oid response)))))
       (testing "not filter julkaistu valintaperuste"
         (let [response (get-ok (hakukohde-url hakukohdeOid1))]
           (is (= true (contains? response :valintaperuste)))))
