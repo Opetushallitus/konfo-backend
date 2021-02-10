c57214f249cb392f540d91ab48cf307195c0b0b0
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-02-10 15:48:07 +0200

    KTO-443: Testi valintaperusteen draftille kun tallennettu ja esikatselu päällä

--- test/konfo_backend/index/hakukohde_test.clj
+++ test/konfo_backend/index/hakukohde_test.clj
@@ -25,7 +25,8 @@
         hakukohdeOid4 "1.2.246.562.20.000004"
         hakukohdeOid5 "1.2.246.562.20.000005"
         valintaperusteId1 "2d0651b7-cdd3-463b-80d9-303a60d9616c"
-        valintaperusteId2 "45d2ae02-9a5f-42ef-8148-47d07737927b"]
+        valintaperusteId2 "3456ae02-9a5f-42ef-8148-47d077373333"
+        valintaperusteId3 "45d2ae02-9a5f-42ef-8148-47d07737927b"]
 
     (fixture/add-haku-mock "1.2.246.562.29.000001" :tila "julkaistu" :organisaatio mocks/Oppilaitos1)
 
@@ -34,10 +35,11 @@
     (fixture/add-hakukohde-mock hakukohdeOid1 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "julkaistu" :esikatselu "false" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId1)
     (fixture/add-hakukohde-mock hakukohdeOid2 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "julkaistu" :esikatselu "false" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId2)
     (fixture/add-hakukohde-mock hakukohdeOid3 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "tallennettu" :esikatselu "false" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId1)
-    (fixture/add-hakukohde-mock hakukohdeOid5 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "tallennettu" :esikatselu "true" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId2)
+    (fixture/add-hakukohde-mock hakukohdeOid5 "1.2.246.562.17.000001" "1.2.246.562.29.000001" :tila "tallennettu" :esikatselu "true" :organisaatio mocks/Oppilaitos1 :valintaperuste valintaperusteId3)
 
     (fixture/add-valintaperuste-mock valintaperusteId1 :tila "julkaistu" :esikatselu "false")
     (fixture/add-valintaperuste-mock valintaperusteId2 :tila "tallennettu" :esikatselu "false")
+    (fixture/add-valintaperuste-mock valintaperusteId3 :tila "tallennettu" :esikatselu "true")
 
     (fixture/add-toteutus-mock "1.2.246.562.17.000001" "1.2.246.562.13.000001")
     (fixture/add-koulutus-mock "1.2.246.562.13.000001")
@@ -63,4 +65,7 @@
           (is (= true (contains? response :valintaperuste)))))
       (testing "filter julkaisematon valintaperuste when draft false"
         (let [response (get-ok (hakukohde-url hakukohdeOid2))]
-          (is (= false (contains? response :valintaperuste) )))))))
+          (is (= false (contains? response :valintaperuste)))))
+      (testing "get draft valintaperuste when tallennettu and esikatselu true"
+        (let [response (get-ok (hakukohde-draft-url hakukohdeOid5))]
+          (is (= true (contains? response :valintaperuste))))))))
