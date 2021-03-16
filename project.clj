4e0662fa7ca282b1fb21000af4ddd9cf1d3257d5
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-03-16 11:43:21 +0200

    KTO-1119: Nostettu kouta-backendin ja indeksoijan versioita

--- project.clj
+++ project.clj
@@ -47,18 +47,18 @@
                    :jvm-opts ["-Dport=3006"]}
              :updater {:jvm-opts ["-Dmode=updater" "-Dport=3006"]}
              :test {:dependencies [[ring/ring-mock "0.3.2"]
-                                   [kouta-indeksoija-service "2.0.0-SNAPSHOT"]
-                                   [fi.oph.kouta/kouta-backend "3.0.0-SNAPSHOT"]
-                                   [fi.oph.kouta/kouta-backend "3.0.0-SNAPSHOT" :classifier "tests"]
+                                   [kouta-indeksoija-service "3.0.0-SNAPSHOT"]
+                                   [fi.oph.kouta/kouta-backend "4.0.0-SNAPSHOT"]
+                                   [fi.oph.kouta/kouta-backend "4.0.0-SNAPSHOT" :classifier "tests"]
                                    [fi.oph.kouta/kouta-common "2.1.0-SNAPSHOT" :classifier "tests"]
                                    [org.mockito/mockito-core "2.28.2"]
                                    [oph/clj-test-utils "0.2.8-SNAPSHOT"]]
                     :injections [(require '[clj-test-utils.elasticsearch-docker-utils :as utils])
                                  (utils/global-docker-elastic-fixture)]}
              :ci-test {:dependencies [[ring/ring-mock "0.3.2"]
-                                      [kouta-indeksoija-service "2.0.0-SNAPSHOT"]
-                                      [fi.oph.kouta/kouta-backend "3.0.0-SNAPSHOT"]
-                                      [fi.oph.kouta/kouta-backend "3.0.0-SNAPSHOT" :classifier "tests"]
+                                      [kouta-indeksoija-service "3.0.0-SNAPSHOT"]
+                                      [fi.oph.kouta/kouta-backend "4.0.0-SNAPSHOT"]
+                                      [fi.oph.kouta/kouta-backend "4.0.0-SNAPSHOT" :classifier "tests"]
                                       [fi.oph.kouta/kouta-common "2.1.0-SNAPSHOT" :classifier "tests"]
                                       [org.mockito/mockito-core "2.28.2"]
                                       [oph/clj-test-utils "0.2.8-SNAPSHOT"]]
