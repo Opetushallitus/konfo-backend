7a5400ceda864478aff0bd18d91dec3c00788306
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-04-30 08:30:13 +0300

    KTO-1133: Haetaan elasticista myös korkeakouluihin liittyvät koulutustyypit. Yläkategoria koulutustyyppi_3 tulee koodistopalavelusta ja alakategoriat on kovakoodattuja arvoja joille löytyy vastinparit indeksoijasta.

--- src/konfo_backend/search/query.clj
+++ src/konfo_backend/search/query.clj
@@ -120,7 +120,7 @@
 
 (defn- koulutustyyppi-filters
   [field]
-  (->filters-aggregation field '["amm" "amm-tutkinnon-osa" "amm-osaamisala"]))
+  (->filters-aggregation field '["amm" "amm-tutkinnon-osa" "amm-osaamisala" "amk" "amk-ylempi" "kandi" "kandi-ja-maisteri" "maisteri" "tohtori"]))
 
 (defn- koulutustyyppi-filters-for-subentity
   [field]
