cd8d14a01e33d60513865115e10b942047ac6db0
Vesa Ruuskanen <vesa.ruuskanen@gofore.com>
2021-05-04 09:47:34 +0300

    KTO-1133: Lisätty maisterifiltteri

--- src/konfo_backend/search/filters.clj
+++ src/konfo_backend/search/filters.clj
@@ -28,6 +28,7 @@
         amk-ylempi-count (get filter-counts :amk-ylempi 0)
         kandi-count (get filter-counts :kandi 0)
         kandi-ja-maisteri-count (get filter-counts :kandi-ja-maisteri 0)
+        maisteri-count (get filter-counts :maisteri 0)
         tohtori-count (get filter-counts :tohtori 0)]
     {:amm         (cond-> {:alakoodit (select-keys koulutustyyppi-info-and-counts [:koulutustyyppi_1 :koulutustyyppi_4 :koulutustyyppi_11 :koulutustyyppi_12])}
                           ammatillinen-count (assoc :count ammatillinen-count))
@@ -35,6 +36,7 @@
                                        :amk-ylempi        {:count amk-ylempi-count}
                                        :kandi             {:count kandi-count}
                                        :kandi-ja-maisteri {:count kandi-ja-maisteri-count}
+                                       :maisteri          {:count maisteri-count}
                                        :tohtori           {:count tohtori-count}}}
                           korkeakoulu-count (assoc :count korkeakoulu-count))}))
 
