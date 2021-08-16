e877d6f78ffe23bb4a998c945ac9a86eb4d0a327
Jussi Jartamo <jussi.jartamo@gofore.com>
2021-08-16 14:44:41 +0300

    debug print only

--- src/konfo_backend/tools.clj
+++ src/konfo_backend/tools.clj
@@ -13,7 +13,7 @@
 (defn log-pretty
   [json]
   (when debug-pretty
-    (log/warn (cheshire/generate-string json {:pretty true}))))
+    (log/debug (cheshire/generate-string json {:pretty true}))))
 
 (defn reduce-merge-map
   [f coll]
