8fe553193f8f3e69f5cd1967d6cdc506375e0922
Tatu Honkanen <tatu.honkanen@cybercom.com>
2021-09-13 09:42:15 +0300

    OY-3052 : Search terms added

--- project.clj
+++ project.clj
@@ -49,7 +49,7 @@
                    :jvm-opts ["-Dport=3006"]}
              :updater {:jvm-opts ["-Dmode=updater" "-Dport=3006"]}
              :test {:dependencies [[ring/ring-mock "0.3.2"]
-                                   [kouta-indeksoija-service "9.0.1-SNAPSHOT"]
+                                   [kouta-indeksoija-service "8.3.0-SNAPSHOT"]
                                    [fi.oph.kouta/kouta-backend "6.16.0-SNAPSHOT"]
                                    [fi.oph.kouta/kouta-backend "6.16.0-SNAPSHOT" :classifier "tests"]
                                    [fi.oph.kouta/kouta-common "2.5.0-SNAPSHOT" :classifier "tests"]
@@ -63,7 +63,7 @@
                                      (intern 'clj-elasticsearch.elastic-utils 'elastic-host (str "http://127.0.0.1:" elasticPort)))
                                    (utils/global-docker-elastic-fixture))]}
              :ci-test {:dependencies [[ring/ring-mock "0.3.2"]
-                                      [kouta-indeksoija-service "9.0.1-SNAPSHOT"]
+                                      [kouta-indeksoija-service "8.3.0-SNAPSHOT"]
                                       [fi.oph.kouta/kouta-backend "6.16.0-SNAPSHOT"]
                                       [fi.oph.kouta/kouta-backend "6.16.0-SNAPSHOT" :classifier "tests"]
                                       [fi.oph.kouta/kouta-common "2.5.0-SNAPSHOT" :classifier "tests"]
