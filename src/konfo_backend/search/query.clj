5ee597136ff6ae670a7c6d95b6076c43b141476e
Petteri Tolonen <petteri.tolonen@knowit.fi>
2021-12-07 14:43:26 +0200

    KTO-1408: Poistettu duplikaatit tuva-jutut koulutustyyppi-filters funktiosta

--- src/konfo_backend/search/query.clj
+++ src/konfo_backend/search/query.clj
@@ -194,7 +194,7 @@
 
 (defn- koulutustyyppi-filters
   [field]
-  (->filters-aggregation field '["amm", "amm-muu", "amm-tutkinnon-osa" "amm-osaamisala" "lk" "amk" "yo" "amk-alempi" "amk-ylempi" "kandi" "kandi-ja-maisteri" "maisteri" "tohtori" "tuva-normal" "tuva-erityisopetus" "tuva" "tuva-normal" "tuva-erityisopetus" "telma", "vapaa-sivistystyo", "vapaa-sivistystyo-opistovuosi", "vapaa-sivistystyo-muu"]))
+  (->filters-aggregation field '["amm", "amm-muu", "amm-tutkinnon-osa" "amm-osaamisala" "lk" "amk" "yo" "amk-alempi" "amk-ylempi" "kandi" "kandi-ja-maisteri" "maisteri" "tohtori" "tuva" "tuva-normal" "tuva-erityisopetus" "telma", "vapaa-sivistystyo", "vapaa-sivistystyo-opistovuosi", "vapaa-sivistystyo-muu"]))
 
 (defn- koulutustyyppi-filters-for-subentity
   [field]
