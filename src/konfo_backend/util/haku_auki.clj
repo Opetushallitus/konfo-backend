88bb319c7596d62c4314453aed73957fb5cb217c
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-01-12 15:15:52 +0200

    with zone

--- src/konfo_backend/util/haku_auki.clj
+++ src/konfo_backend/util/haku_auki.clj
@@ -2,17 +2,21 @@
   (:require
     [clojure.walk :refer [prewalk]])
   (:import [org.joda.time.format DateTimeFormat]
-           [org.joda.time DateTime]))
+           [org.joda.time DateTime DateTimeZone]))
 
 (def DATE_FORMAT (DateTimeFormat/forPattern "yyyy-MM-dd'T'hh:mm"))
+(def ZONE (DateTimeZone/forID "Europe/Helsinki"))
 
 (defn with-is-haku-auki [data]
   (let [now (DateTime/now)]
     (prewalk (fn [x]
                (if-let [alkaa (:alkaa x)]
-                 (let [before-alkaa (.isBefore (.parseDateTime DATE_FORMAT alkaa) now)]
+                 (let [before-alkaa (.isBefore (.withZone (.parseDateTime DATE_FORMAT alkaa) ZONE) now)]
                    (-> x
                        (assoc :haku-auki (boolean (if-let [loppuu (:loppuu x)]
-                                                    (and before-alkaa (.isAfter (.parseDateTime DATE_FORMAT loppuu) now))
+                                                    (and before-alkaa (.isAfter
+                                                                        (.withZone
+                                                                          (.parseDateTime DATE_FORMAT loppuu)
+                                                                          ZONE) now))
                                                     before-alkaa)))))
                  x)) data)))
