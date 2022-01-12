37840730fda71dc975a6186828cf488a088fb534
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-01-12 15:48:50 +0200

    use localtime

--- src/konfo_backend/util/haku_auki.clj
+++ src/konfo_backend/util/haku_auki.clj
@@ -2,19 +2,18 @@
   (:require
     [clojure.walk :refer [prewalk]])
   (:import [org.joda.time.format DateTimeFormat]
-           [org.joda.time DateTime DateTimeZone]))
+           (java.time LocalDate)))
 
 (def DATE_FORMAT (DateTimeFormat/forPattern "yyyy-MM-dd'T'hh:mm"))
-(def ZONE (DateTimeZone/forID "Europe/Helsinki"))
 
 (defn with-is-haku-auki [data]
-  (let [now (.withZone (DateTime/now) ZONE)]
+  (let [now (LocalDate/now)]
     (prewalk (fn [x]
                (if-let [alkaa (:alkaa x)]
-                 (let [before-alkaa (.isBefore (.parseDateTime DATE_FORMAT alkaa) now)]
+                 (let [before-alkaa (.isBefore (.parseLocalTime DATE_FORMAT alkaa) now)]
                    (-> x
                        (assoc :haku-auki (boolean (if-let [loppuu (:loppuu x)]
                                                     (and before-alkaa (.isAfter
-                                                                          (.parseDateTime DATE_FORMAT loppuu) now))
+                                                                          (.parseLocalTime DATE_FORMAT loppuu) now))
                                                     before-alkaa)))))
                  x)) data)))
