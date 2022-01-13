1857fc0794ed7922d6c85082874da0addc8f35f2
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-01-13 07:37:33 +0200

    using datetime

--- src/konfo_backend/util/haku_auki.clj
+++ src/konfo_backend/util/haku_auki.clj
@@ -1,19 +1,20 @@
 (ns konfo-backend.util.haku-auki
   (:require
-    [clojure.walk :refer [prewalk]])
-  (:import [org.joda.time.format DateTimeFormat]
-           (java.time LocalDateTime)))
+    [clojure.walk :refer [prewalk]]
+    [clj-time.format :as f])
+  (:import (org.joda.time DateTime)))
 
-(def DATE_FORMAT (DateTimeFormat/forPattern "yyyy-MM-dd'T'hh:mm"))
+(def FORMATTER (f/formatter "yyyy-MM-dd'T'hh:mm"))
 
 (defn with-is-haku-auki [data]
-  (let [now (LocalDateTime/now)]
+  (let [now (DateTime/now)]
     (prewalk (fn [x]
-               (if-let [alkaa (:alkaa x)]
-                 (let [before-alkaa (.isBefore (.parseLocalDateTime DATE_FORMAT alkaa) now)]
+               (if-let [alkaa (some->> (:alkaa x)
+                                       (f/parse FORMATTER))]
+                 (let [before-alkaa (.isBefore alkaa now)]
                    (-> x
-                       (assoc :haku-auki (boolean (if-let [loppuu (:loppuu x)]
-                                                    (and before-alkaa (.isAfter
-                                                                          (.parseLocalDateTime DATE_FORMAT loppuu) now))
+                       (assoc :haku-auki (boolean (if-let [loppuu (some->> (:loppuu x)
+                                                                           (f/parse FORMATTER))]
+                                                    (and before-alkaa (.isAfter loppuu now))
                                                     before-alkaa)))))
                  x)) data)))
