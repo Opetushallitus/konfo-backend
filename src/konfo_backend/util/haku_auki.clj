14aa2c32608f79e12e61876809fbc69df69ac55e
Jussi Jartamo <jussi.jartamo@gofore.com>
2022-01-13 09:18:15 +0200

    check simplified

--- src/konfo_backend/util/haku_auki.clj
+++ src/konfo_backend/util/haku_auki.clj
@@ -7,14 +7,16 @@
 (def FORMATTER (f/formatter "yyyy-MM-dd'T'HH:mm"))
 
 (defn with-is-haku-auki [data]
-  (let [now (DateTime/now)]
+  (let [now (DateTime/now)
+        on-going? (fn [d ^DateTime]
+                    (.isBefore d now))]
     (prewalk (fn [x]
                (if-let [alkaa (some->> (:alkaa x)
                                        (f/parse FORMATTER))]
-                 (let [before-alkaa (.isAfter alkaa now)]
-                   (-> x
-                       (assoc :haku-auki (boolean (if-let [loppuu (some->> (:loppuu x)
-                                                                           (f/parse FORMATTER))]
-                                                    (and before-alkaa (.isBefore loppuu now))
-                                                    before-alkaa)))))
+                 (let [is-alkanut? (on-going? alkaa)]
+                   (assoc x :haku-auki (boolean (if-let [loppuu (some->> (:loppuu x)
+                                                                         (f/parse FORMATTER))]
+                                                  (let [is-loppunut? (on-going? loppuu)]
+                                                    (and is-alkanut? (not is-loppunut?)))
+                                                  is-alkanut?))))
                  x)) data)))
