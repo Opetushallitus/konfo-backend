8413cb7c865709759be801d60f729d69d3a6d5b8
Matti Pitkämäki <matti.pitkamaki@gofore.com>
2022-02-15 10:59:20 +0200

    KTO-201 & KTO-1347: Lisätty testikonfiguraatio

--- project.clj
+++ project.clj
@@ -58,7 +58,8 @@
                                    [org.mockito/mockito-all "1.9.5"]
                                    [clj-http-fake "1.0.3"]
                                    [pjstadig/humane-test-output "0.11.0"]]
-                    :injections [(require '[clj-test-utils.elasticsearch-docker-utils :as utils])
+             :jvm-opts ["-Dlog4j.configurationFile=test/resources/log4j2.properties" "-Dconf=dev-configuration/konfo-backend.edn"]
+             :injections [(require '[clj-test-utils.elasticsearch-docker-utils :as utils])
                                  (require '[clj-elasticsearch.elastic-utils :as eutils])
                                  (if-let [elasticPort (java.lang.System/getenv "elasticPort")]
                                    (do
