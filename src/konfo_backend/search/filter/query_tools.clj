7578d4d35e2e39f1f1f6639a00919064d1a0b111
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-03-27 09:50:55 +0300

    OPHJOD-88: Korjattu sykliset riippuvuudet

--- src/konfo_backend/search/filter/query_tools.clj
+++ src/konfo_backend/search/filter/query_tools.clj
@@ -1,7 +1,29 @@
 (ns konfo-backend.search.filter.query-tools
-  (:require
-    [konfo-backend.search.tools :refer [->terms-query lukiopainotukset? lukiolinjaterityinenkoulutustehtava? osaamisala?]]
-    ))
+  (:require [clojure.string :refer [lower-case]]
+            [konfo-backend.tools :refer [->lower-case-vec]]))
+
+(defn constraint?
+  [constraints key]
+  (not (empty? (key constraints))))
+
+(defn lukiopainotukset?
+  [constraints]
+  (constraint? constraints :lukiopainotukset))
+
+(defn lukiolinjaterityinenkoulutustehtava?
+  [constraints]
+  (constraint? constraints :lukiolinjaterityinenkoulutustehtava))
+
+(defn osaamisala?
+  [constraints]
+  (constraint? constraints :osaamisala))
+
+(defn ->terms-query
+  [key coll]
+  (let [search-field (keyword (str "search_terms." key))]
+    (if (= 1 (count coll))
+      {:term {search-field (lower-case (first coll))}}
+      {:terms {search-field (->lower-case-vec coll)}})))
 
 (defn keyword-terms-query
   [field coll]
@@ -28,8 +50,8 @@
 
 (defn- lukio-filters [constraints]
   (cond-> []
-          (lukiopainotukset? constraints) (conj (->terms-query "lukiopainotukset" (:lukiopainotukset constraints)))
-          (lukiolinjaterityinenkoulutustehtava? constraints) (conj (->terms-query "lukiolinjaterityinenkoulutustehtava" (:lukiolinjaterityinenkoulutustehtava constraints)))))
+    (lukiopainotukset? constraints) (conj (->terms-query "lukiopainotukset" (:lukiopainotukset constraints)))
+    (lukiolinjaterityinenkoulutustehtava? constraints) (conj (->terms-query "lukiolinjaterityinenkoulutustehtava" (:lukiolinjaterityinenkoulutustehtava constraints)))))
 
 (defn- osaamisala-filters [constraints]
   [(->terms-query "osaamisalat" (:osaamisala constraints))])
@@ -45,3 +67,17 @@
     (if (empty? filters)
       {}
       {:should filters})))
+
+(defn- some-hakuaika-kaynnissa
+  [current-time]
+  {:should [{:bool {:filter [{:range {:search_terms.toteutusHakuaika.alkaa {:lte current-time}}}
+                             {:bool {:should [{:bool {:must_not {:exists {:field "search_terms.toteutusHakuaika.paattyy"}}}},
+                                              {:range {:search_terms.toteutusHakuaika.paattyy {:gt current-time}}}]}}]}}
+            {:nested {:path  "search_terms.hakutiedot.hakuajat"
+                      :query {:bool {:filter [{:range {:search_terms.hakutiedot.hakuajat.alkaa {:lte current-time}}}
+                                              {:bool {:should [{:bool {:must_not {:exists {:field "search_terms.hakutiedot.hakuajat.paattyy"}}}},
+                                                               {:range {:search_terms.hakutiedot.hakuajat.paattyy {:gt current-time}}}]}}]}}}}]})
+
+(defn hakuaika-filter-query
+  [current-time]
+  {:bool (some-hakuaika-kaynnissa current-time)})
