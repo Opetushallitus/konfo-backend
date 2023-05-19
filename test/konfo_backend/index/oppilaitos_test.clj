5f05b8d88e6fbacc386170e213a0da085487e300
Petteri Tolonen <petteri.tolonen@knowit.fi>
2023-05-19 08:19:34 +0300

    OY-4226: Päivitetty elasticdumpit, korjattu koulutusohjelmien lukumäärän tarkistus oppilaitos-testeissä ja formatoitu oppilaitos-testien koodi

--- test/konfo_backend/index/oppilaitos_test.clj
+++ test/konfo_backend/index/oppilaitos_test.clj
@@ -1,6 +1,6 @@
 (ns konfo-backend.index.oppilaitos-test
   (:require [clojure.test :refer :all]
-            [clj-elasticsearch.elastic-utils :refer [elastic-post]]
+            [matcher-combinators.test]
             [konfo-backend.test-tools :refer :all]))
 
 (intern 'clj-log.access-log 'service "konfo-backend")
@@ -15,6 +15,10 @@
   [oid]
   (apply url-with-query-params (str "/konfo-backend/oppilaitos/" oid) [:draft true]))
 
+(defn find-osa
+  [response oid]
+  (first (filter #(= oid (:oid %)) (:osat response))))
+
 (deftest oppilaitos-test
 
   (let [oppilaitosOid1  "1.2.246.562.10.00101010101"
@@ -28,16 +32,13 @@
         oppilaitoksenOsaOid4 "1.2.246.562.10.001010101022"
         oppilaitoksenOsaOid5 "1.2.246.562.10.001010101023"]
 
-    (defn find-osa
-      [response oid]
-      (first (filter #(= oid (:oid %)) (:osat response))))
-
     (testing "Get oppilaitos"
       (testing "ok"
         (let [response (get-ok (oppilaitos-url oppilaitosOid7))]
           (is (= oppilaitosOid7 (:oid response)))
           (is (contains? response :oppilaitos))
-          (is (= 0 (:koulutusohjelmia response)))))
+          (is (match? {:kaikki 0 :tutkintoonJohtavat 0 :eiTutkintoonJohtavat 0}
+                      (:koulutusohjelmatLkm response)))))
       (testing "allowed to get draft oppilaitos when tallennettu and esikatselu true"
         (let [response (get-ok (oppilaitos-draft-url oppilaitosOid4))]
           (is (= oppilaitosOid4 (:oid response)))
@@ -59,18 +60,15 @@
       (testing "allowed to get julkaistu and not esikatselu oppilaitoksen osa in Kouta"
         (let [response (get-ok (oppilaitos-url oppilaitosOid1))]
           (is (= oppilaitosOid1 (:oid response)))
-            (is (contains? (find-osa response oppilaitoksenOsaOid1) :oppilaitoksenOsa))
-          ))
+          (is (contains? (find-osa response oppilaitoksenOsaOid1) :oppilaitoksenOsa))))
       (testing "filter not julkaistu and not esikatselu oppilaitoksen osa in Kouta"
         (let [response (get-ok (oppilaitos-url oppilaitosOid1))]
           (is (= oppilaitosOid1 (:oid response)))
-          (is (false? (contains? (find-osa response oppilaitoksenOsaOid2) :oppilaitoksenOsa)))
-          ))
+          (is (false? (contains? (find-osa response oppilaitoksenOsaOid2) :oppilaitoksenOsa)))))
       (testing "filter draft oppilaitoksen osa when tallennettu but esikatselu false"
         (let [response (get-ok (oppilaitos-draft-url oppilaitosOid2))]
           (is (= oppilaitosOid2 (:oid response)))
-          (is (false? (contains? (find-osa response oppilaitoksenOsaOid5) :oppilaitoksenOsa)))
-          ))
+          (is (false? (contains? (find-osa response oppilaitoksenOsaOid5) :oppilaitoksenOsa)))))
       (testing "filter oppilaitoksen osa that is tallennettu and esikatselu true but draft false"
         (let [response (get-ok (oppilaitos-url oppilaitosOid2))]
           (is (= oppilaitosOid2 (:oid response)))
@@ -80,4 +78,4 @@
           (is (= oppilaitosOid2 (:oid response)))
           ;;TODO alla oleva kommentoitu elastic7 -testauksen yhteydessä. Täytyy kuitenkin myöhemmin selvittää / korjata
           ;;(is (true? (contains? (find-osa response oppilaitoksenOsaOid4) :oppilaitoksenOsa)))
-        )))))
+          )))))
