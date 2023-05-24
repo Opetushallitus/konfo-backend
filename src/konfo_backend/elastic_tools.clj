386b1dc5262a03479494caaddbc26ff6c0c52ec6
Risto Salama <risto.salama@gofore.com>
2023-05-24 14:16:56 +0300

    Fix search-after to work properly and short circuit if total is less than sum

--- src/konfo_backend/elastic_tools.clj
+++ src/konfo_backend/elastic_tools.clj
@@ -1,10 +1,7 @@
 (ns konfo-backend.elastic-tools
   (:require
     [clj-elasticsearch.elastic-connect :as e]
-    [clj-elasticsearch.elastic-utils :as u]
-    [clj-log.error-log :refer [with-error-logging]]
-    [clojure.string :as str]
-    [clojure.tools.logging :as log]))
+    [clojure.string :as str]))
 
 (def limit-to-use-search-after 10000)
 
@@ -13,7 +10,7 @@
 (defn get-source
   ([index id excludes]
    (when id
-     (let [result (e/get-document index id :_source_excludes (clojure.string/join "," excludes))]
+     (let [result (e/get-document index id :_source_excludes (str/join "," excludes))]
        (when (:found result)
          (:_source result)))))
   ([index id]
@@ -22,16 +19,17 @@
 (defn get-sources
   [index ids excludes]
   (if (seq excludes)
-    (e/multi-get index ids :_source_excludes (clojure.string/join "," excludes))
+    (e/multi-get index ids :_source_excludes (str/join "," excludes))
     (e/multi-get index ids)))
+
+(defn search-without-mapper
+  [index & query-parts]
+  (let [query-parts-without-nils (apply concat (remove (fn [[_ v]] (nil? v)) (partition 2 query-parts)))]
+    (apply e/search index query-parts-without-nils)))
 
 (defn search
   [index mapper & query-parts]
-  (let [query-parts-without-nils (apply concat (remove (fn [[_ v]] (nil? v)) (partition 2 query-parts)))]
-    (->> (apply e/search
-                index
-                query-parts-without-nils)
-         mapper)))
+  (mapper (search-without-mapper index query-parts)))
 
 (defn count
   [index & query-parts]
@@ -50,26 +48,31 @@
 ; https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
 (defn do-search-after [search-fn index mapper from size query-parts]
   (let [initial-from   (- limit-to-use-search-after size)
-        initial-search (apply search-fn index mapper :from initial-from :size size query-parts)
-        search-with-do-after (fn [sort from-index]
-                               (apply search-fn index mapper :from from-index :size size :search_after sort query-parts))
+        initial-search (apply search-fn index :from initial-from :size size query-parts)
+        search-with-do-after (fn [sort]
+                               (apply search-fn index :from 0 :size size :search_after sort query-parts))
         take-last-sort (fn [results] (->> (:hits results)
                                           :hits
                                           last
                                           :sort))
-        next-result    (fn [last-sort from-index] (search-with-do-after last-sort from-index))
         last-sort      (atom (take-last-sort initial-search))
         final-result (atom initial-search)]
-    (doseq [multiplier (range (/ (- from limit-to-use-search-after) size))]
-      (let [result (next-result @last-sort (+ initial-from (* (+ multiplier 1) size)))]
-        (reset! last-sort (take-last-sort result))
-        (reset! final-result result)))
-    @final-result))
+    (if (< (get-in initial-search [:hits :total :value] 0) from)
+      (-> initial-search
+          (assoc :hits [])
+          mapper)
+      (do
+        (doseq [_ (range (/ (- from limit-to-use-search-after) size))]
+          (when (not (nil? @last-sort))
+            (let [result (search-with-do-after @last-sort)]
+              (reset! last-sort (take-last-sort result))
+              (reset! final-result result))))
+        (mapper @final-result)))))
 
 (defn search-with-pagination
   [index page size mapper & query-parts]
   (let [size (->size size)
         from (->from page size)]
-    (if (< limit-to-use-search-after from)
-      (do-search-after search index mapper from size query-parts)
+    (if (< limit-to-use-search-after (+ from size))
+      (do-search-after search-without-mapper index mapper from size query-parts)
       (apply search index mapper :from from :size size query-parts))))
--- test/konfo_backend/elastic_tools_test.clj
+++ test/konfo_backend/elastic_tools_test.clj
@@ -5,18 +5,29 @@
 (defn- create-search-fn [times]
   (fn[index mapper & query-parts]
     (swap! times inc)
-    {:hits {:hits [{:sort [@times (- @times 1)]}]}}))
+    {:hits {:total {:value 99999}
+            :hits [{:sort [@times (- @times 1)]}]}}))
+
+(defn- mapper [input] input)
 
 (deftest elastic-tools-test
   (testing "calls search twice"
     (let [times (atom 0)
           search-fn (create-search-fn times)]
-      (tools/do-search-after search-fn nil nil 10001 200 [])
+      (tools/do-search-after search-fn nil mapper 10001 200 [])
       (is (= @times 2))))
 
   (testing "calls search ten times"
     (let [times (atom 0)
           search-fn (create-search-fn times)
-          result (tools/do-search-after search-fn nil nil 11800 200 [])]
-      (is (= result {:hits {:hits [{:sort [10 9]}]}}))
-      (is (= @times 10)))))
+          result (tools/do-search-after search-fn nil mapper 11800 200 [])]
+      (is (= result {:hits {:total {:value 99999}
+                            :hits [{:sort [10 9]}]}}))
+      (is (= @times 10))))
+
+  (testing "short circuits if from is over total"
+    (let [times (atom 0)
+          search-fn (create-search-fn times)
+          result (tools/do-search-after search-fn nil mapper 100000 10 [])]
+      (is (= result {:hits []}))
+      (is (= @times 1)))))
