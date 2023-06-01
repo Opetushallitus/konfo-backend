606d39613e4ed9b089491f8a6d61fa27fc6370d1
Risto Salama <risto.salama@gofore.com>
2023-06-01 16:40:41 +0300

    Use PIT with search after

--- src/konfo_backend/elastic_tools.clj
+++ src/konfo_backend/elastic_tools.clj
@@ -2,11 +2,15 @@
   (:require
     [clj-elasticsearch.elastic-connect :as e]
     [clj-log.error-log :refer [with-error-logging]]
+    [clj-http.client :as http]
+    [clj-elasticsearch.elastic-utils :refer [elastic-post elastic-url ]]
     [clojure.string :as str]))
 
 (def limit-to-use-search-after 10000)
 
 (def max-size 200)
+
+(def minimum-size-for-search-after 5)
 
 (defn get-source
   ([index id excludes]
@@ -30,11 +34,10 @@
 
 (defn search
   [index mapper & query-parts]
-  (let [query-parts-without-nils (apply concat (remove (fn [[_ v]] (nil? v)) (partition 2 query-parts)))]
-    (->> (apply e/search
-                index
-                query-parts-without-nils)
-         mapper)))
+  (->> (apply search-without-mapper
+              index
+              query-parts)
+       mapper))
 
 (defn count
   [index & query-parts]
@@ -50,50 +53,75 @@
   [page size]
   (if (pos? page) (* (- page 1) size) 0))
 
+(defn- get-pit
+  [index]
+  (let [json-request {:as :json
+                      :content-type :json
+                      :socket-timeout 120000}
+        pit (:body (http/post (elastic-url index "_pit?keep_alive=1m") json-request))]
+    {:id (:id pit)
+     :keep_alive "1m"}))
+
+; https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
 (defn- do-search-after
-  [search-fn index from size query-parts initial-search result-gatherer-fn]
-  (let [search-with-do-after (fn [sort]
-                               (apply search-fn index :from 0 :size size :search_after sort query-parts))
+  [search-fn from size query-parts initial-search result-gatherer-fn]
+  (let [get-pit-from-result (fn [result]
+                              {:id (:pit_id result)
+                               :keep_alive "1m"})
+        search-with-do-after (fn [sort pit-val]
+                               (apply search-fn
+                                      nil
+                                      :pit pit-val
+                                      :from 0
+                                      :size size
+                                      :search_after sort
+                                      :track_total_hits false
+                                      query-parts))
         take-last-sort (fn [results] (->> (:hits results)
                                           :hits
                                           last
-                                          :sort))
-        last-sort      (atom (take-last-sort initial-search))
-        final-result (atom [])]
-    (doseq [_ (range (/ (- from limit-to-use-search-after) size))]
-      (when (not (nil? @last-sort))
-        (let [result (search-with-do-after @last-sort)]
-          (reset! last-sort (take-last-sort result))
-          (result-gatherer-fn final-result result))))
-    @final-result))
+                                          :sort))]
+    (loop [n (clojure.core/count (range (/ (- from limit-to-use-search-after) size)))
+           pit (get-pit-from-result initial-search)
+           last-sort (take-last-sort initial-search)
+           final-result []]
+      (if-let [result (when (and (not (neg? n))
+                                 (some? last-sort))
+                        (search-with-do-after last-sort pit))]
+        (recur (dec n)
+               (get-pit-from-result result)
+               (take-last-sort result)
+               (result-gatherer-fn final-result result))
+        final-result))))
 
-; https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
 (defn do-search-after-concanate-results [search-fn index mapper to size query-parts]
-  (let [initial-from   (- limit-to-use-search-after size)
-        initial-search (apply search-fn index :from initial-from :size size query-parts)]
-        (do-search-after search-fn index to size query-parts initial-search
-                         (fn [atom-obj result] (reset! atom-obj (concat @atom-obj (mapper result)))))))
+  (let [pit (get-pit index)
+        initial-from   (- limit-to-use-search-after size)
+        initial-search (apply search-fn nil :pit pit :from initial-from :size size query-parts)]
+        (do-search-after search-fn to size query-parts initial-search
+                         (fn [final-result result] (concat final-result (mapper result))))))
 
 (defn search-all-with-do-after [index mapper to & query-parts]
   (let [initial-results (apply search index mapper :size limit-to-use-search-after query-parts)
         rest-of-results (do-search-after-concanate-results search-without-mapper index mapper to max-size query-parts)]
     (concat initial-results rest-of-results)))
 
-; https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
 (defn do-search-after-paged [search-fn index mapper from size & query-parts]
-  (let [initial-from   (- limit-to-use-search-after size)
-        initial-search (apply search-fn index :from initial-from :size size query-parts)]
+  (let [size (if (< size minimum-size-for-search-after) minimum-size-for-search-after size)
+        initial-from (- limit-to-use-search-after size)
+        pit (get-pit index)
+        initial-search (apply search-fn nil :pit pit :from initial-from :size size :sort (:sort query-parts) query-parts)]
     (if (< (get-in initial-search [:hits :total :value] 0) from)
       (-> initial-search
           (assoc :hits [])
           mapper)
       (mapper
-        (do-search-after search-fn index from size query-parts initial-search (fn [atom-obj result] (reset! atom-obj result)))))))
+        (do-search-after search-fn from size query-parts initial-search (fn [_ result] result))))))
 
 (defn search-with-pagination
   [index page size mapper & query-parts]
   (let [size (->size size)
         from (->from page size)]
     (if (< limit-to-use-search-after (+ from size))
-      (do-search-after-paged search-without-mapper index mapper from size query-parts)
+      (apply do-search-after-paged search-without-mapper index mapper from size query-parts)
       (apply search index mapper :from from :size size query-parts))))
--- test/konfo_backend/elastic_tools_test.clj
+++ test/konfo_backend/elastic_tools_test.clj
@@ -1,9 +1,10 @@
 (ns konfo-backend.elastic-tools-test
   (:require [clojure.test :refer :all]
+            [clj-http.client :as http]
             [konfo-backend.elastic-tools :as tools]))
 
 (defn- create-search-fn [times]
-  (fn[index mapper & query-parts]
+  (fn [index mapper & query-parts]
     (swap! times inc)
     {:hits {:total {:value 99999}
             :hits [{:sort [@times (- @times 1)]}]}}))
@@ -11,30 +12,31 @@
 (defn- mapper [input] input)
 
 (deftest elastic-tools-test
-  (testing "calls search twice"
-    (let [times (atom 0)
-          search-fn (create-search-fn times)]
-      (tools/do-search-after-paged search-fn nil mapper 10001 200 [])
-      (is (= @times 2))))
+  (with-redefs [http/post {:id "2343242"}]
+    (testing "calls search twice"
+      (let [times (atom 0)
+            search-fn (create-search-fn times)]
+        (tools/do-search-after-paged search-fn nil mapper 10000 200 [])
+        (is (= @times 2))))
 
-  (testing "calls search ten times"
-    (let [times (atom 0)
-          search-fn (create-search-fn times)
-          result (tools/do-search-after-paged search-fn nil mapper 11800 200 [])]
-      (is (= result {:hits {:total {:value 99999}
-                            :hits [{:sort [10 9]}]}}))
-      (is (= @times 10))))
+    (testing "calls search ten times"
+      (let [times (atom 0)
+            search-fn (create-search-fn times)
+            result (tools/do-search-after-paged search-fn nil mapper 11600 200 [])]
+        (is (= result {:hits {:total {:value 99999}
+                              :hits [{:sort [10 9]}]}}))
+        (is (= @times 10))))
 
-  (testing "short circuits if from is over total"
-    (let [times (atom 0)
-          search-fn (create-search-fn times)
-          result (tools/do-search-after-paged search-fn nil mapper 100000 10 [])]
-      (is (= result {:hits []}))
-      (is (= @times 1))))
+    (testing "short circuits if from is over total"
+      (let [times (atom 0)
+            search-fn (create-search-fn times)
+            result (tools/do-search-after-paged search-fn nil mapper 100000 10 [])]
+        (is (= result {:hits []}))
+        (is (= @times 1))))
 
-  (testing "gathers results not just single page"
-    (let [times (atom 0)
-          search-fn (create-search-fn times)
-          result (tools/do-search-after-concanate-results search-fn nil mapper 11000 200 [])]
-      (is (= @times 6))
-      (is (= (count result) 5)))))
+    (testing "gathers results not just single page"
+      (let [times (atom 0)
+            search-fn (create-search-fn times)
+            result (tools/do-search-after-concanate-results search-fn nil mapper 11000 200 [])]
+        (is (= @times 7))
+        (is (= (count result) 6))))))
