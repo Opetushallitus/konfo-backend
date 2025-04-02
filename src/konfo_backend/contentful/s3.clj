97cd576c98a7d223913eab3bc8a40f57ef828eba
Tomi Katajanheimo <tomi@katajanheimo.com>
2025-04-02 16:07:32 +0300

    OY-5050 refaktoroi käyttämään aws sdk v2:ta

--- src/konfo_backend/contentful/s3.clj
+++ src/konfo_backend/contentful/s3.clj
@@ -1,36 +1,35 @@
 (ns konfo-backend.contentful.s3
   (:require
     [konfo-backend.config :refer [config]]
-    [clojure.java.io :as io]
     [clojure.tools.logging :as log])
-  (:import [com.amazonaws.services.s3 AmazonS3Client AmazonS3]
-           [com.amazonaws.services.s3.model
-            PutObjectRequest
-            ObjectMetadata
-            CannedAccessControlList]
-           [org.joda.time DateTime]
-           [com.amazonaws.regions Regions]
-           [com.amazonaws.auth DefaultAWSCredentialsProviderChain]
-           [com.amazonaws.auth.profile ProfileCredentialsProvider]
+  (:import (software.amazon.awssdk.core.sync RequestBody)
+           (software.amazon.awssdk.core.sync ResponseTransformer)
+           (software.amazon.awssdk.services.s3 S3Client)
+           (software.amazon.awssdk.services.s3.model GetObjectRequest HeadObjectRequest PutObjectRequest NoSuchKeyException ObjectCannedACL)
+           (software.amazon.awssdk.regions Region)
+           (software.amazon.awssdk.auth.credentials
+            DefaultCredentialsProvider
+            ProfileCredentialsProvider)
+           (java.time Instant)
+           (java.time.temporal ChronoUnit)
            (java.security MessageDigest)
-           (org.apache.commons.codec.binary Hex)
-           (java.util Base64)))
+           (org.apache.commons.codec.binary Hex)))
 
 (defn- credentials-provider [config]
   (if-let [profile-name (get-in config [:s3 :credentials-profile])]
-    (new ProfileCredentialsProvider profile-name)
-    (DefaultAWSCredentialsProviderChain/getInstance)))
+    (ProfileCredentialsProvider/create profile-name)
+    (DefaultCredentialsProvider/create)))
 
 (defn- region [config]
   (if-let [region (get-in config [:s3 :region])]
-    (Regions/fromName region)
+    (Region/of region)
     (log/warn "No region name configured!")))
 
 (defn create-client []
   (when-let [region (region config)]
-    (-> (AmazonS3Client/builder)
-        (.withRegion region)
-        (.withCredentials (credentials-provider config))
+    (-> (S3Client/builder)
+        (.region region)
+        (.credentialsProvider (credentials-provider config))
         (.build))))
 
 (defonce md5-algorithm (MessageDigest/getInstance "MD5"))
@@ -39,37 +38,51 @@
   (Hex/encodeHexString
     (.digest md5-algorithm data)))
 
-(defn put-object [ttl-hours s3-client file-key data content-type]
+(defn put-object [ttl-hours ^S3Client s3-client file-key data content-type]
   (try
     (let [bucket-name (-> config :s3 :bucket-name)
           bytes       data
-          metadata    (doto (ObjectMetadata.)
-                        (.setHttpExpiresDate (.toDate (.plusHours (DateTime/now) ttl-hours)))
-                        (.setContentType content-type)
-                        (.setContentLength (alength bytes)))
-          request     (-> (PutObjectRequest. bucket-name
-                            file-key
-                            (io/input-stream bytes)
-                            metadata)
-                          (.withCannedAcl CannedAccessControlList/PublicRead))]
-      (.putObject s3-client request))
+          request     (-> (PutObjectRequest/builder)
+                          (.bucket bucket-name)
+                          (.key file-key)
+                          (.expires
+                            (.plus (Instant/now)
+                                   (long ttl-hours)
+                                   (ChronoUnit/HOURS)))
+                          (.contentType content-type)
+                          (.contentLength (long (alength bytes)))
+                          (.acl (str (ObjectCannedACL/PUBLIC_READ)))
+                          (.build))]
+      (.putObject s3-client request
+                  (RequestBody/fromBytes bytes)))
     (catch Exception e
       (log/error
-        (str "Unable to put object " file-key " to bucket " (-> config :s3 :bucket-name) "! " e))
+        (str "Unable to put object " file-key
+             " to bucket " (-> config :s3 :bucket-name) "! " e))
       (throw e))))
 
-(defn get-object [^AmazonS3 s3-client ^String file-key]
+(defn get-object [^S3Client s3-client ^String file-key]
   (when (and file-key s3-client)
         (try
-          (-> (.getObject s3-client (-> config :s3 :bucket-name) file-key)
-              (.getObjectContent)
+          (-> s3-client
+              (.getObject (-> (GetObjectRequest/builder)
+                              (.bucket (-> config :s3 :bucket-name))
+                              (.key file-key)
+                              (.build))
+                          (ResponseTransformer/toBytes))
+              (.asByteArray)
               (slurp))
-          (catch Exception e
+          (catch NoSuchKeyException e
             (log/error (str "Key not found " file-key "!" e))))))
 
 (defn get-object-md5 [s3-client file-key]
   (try
-    (when-let [meta-data (.getObjectMetadata s3-client (-> config :s3 :bucket-name) file-key)]
-      (.getRawMetadataValue meta-data "ETag"))
-    (catch Exception e
+    (-> s3-client
+        (.headObject (-> (HeadObjectRequest/builder)
+                         (.bucket
+                           (-> config :s3 :bucket-name))
+                         (.key file-key)
+                         (.build)))
+        (.eTag))
+    (catch NoSuchKeyException e
       (log/info (str "File doesn't exist " file-key "!")))))
